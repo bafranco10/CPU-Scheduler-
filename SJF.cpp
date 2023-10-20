a101c5bded02235a977fef1fa498f7a89e035deb
estemora <138655765+estemora@users.noreply.github.com>
2023-10-20 12:03:24 -0400

    Add files via upload

--- SJF.cpp
+++ SJF.cpp
@@ -8,7 +8,7 @@
 #include "SJF.h"
 #include "FCFS.h"
 #include "PCB_Class.h"
-
+#include <map>
 using namespace std;
 
 SJF::SJF(){
@@ -17,61 +17,83 @@ SJF::SJF(){
 SJF::~SJF(){
 }
 
+// somewhat working, verbose is not correct and gives wrong answer if processes arrive at same time
+//  print it says the wait time is the cpu time where it stopped executing
 void SJF::sjfSchedule(bool verbose, bool preemption, PCB_Class& pcb) {
-    FCFS fcfs;
-    PCB_Class::PCB block;
-    int cpuTime = 0;
-    int initSize = pcb.queueSize(pcb.initQueue); //need to initialize size of the queue to be used
-
     if (!preemption) {
-        // non-preemptive SJF using FCFS functions
+        // Non-preemptive SJF using FCFS functions
+        FCFS fcfs;
         fcfs.fcfsSchedule(verbose, pcb, pcb.BURST_TAG);
-    } 
+        return;
+    }
 
-    else if (preemption) {
-        while (pcb.queueSize(pcb.doneQueue) != initSize) {
-            // put process in ready queue if queue is not empty
-            if (!pcb.queueEmpty(pcb.initQueue)) {
-                pcb.queueTagSort(pcb.initQueue, pcb.BURST_TAG);
-                pcb.makeReady(pcb.initQueue, cpuTime);
-            }
+    int cpuTime = 0;
+    PCB_Class::PCB currentProcess;
+    bool isProcessRunning = false;
 
-            // if ready queue is empty but initQueue is not, make cpuTime the arrival time of next process
-            if (pcb.queueEmpty(pcb.readyQueue) && !pcb.queueEmpty(pcb.initQueue)) {
-                pcb.queueTagSort(pcb.initQueue, pcb.ARRIVAL_TAG);
-                cpuTime = pcb.findTagPCB(pcb.initQueue, pcb.ARRIVAL_TAG).arrivalTime;
-            } 
-            
-            else {
-                pcb.queueTagSort(pcb.readyQueue, pcb.BURST_TAG);
-                block = pcb.getPCB(pcb.readyQueue);
+    pcb.queueTagSort(pcb.initQueue, pcb.ARRIVAL_TAG);
 
-                // preempt the following process if the current one has not finished
-                if (block.arrivalTime > cpuTime) {
-                    cpuTime = block.arrivalTime;
+    while (!pcb.queueEmpty(pcb.initQueue) || !pcb.queueEmpty(pcb.readyQueue) || isProcessRunning) {
+
+        // Move processes from initQueue to readyQueue based on arrival time
+        pcb.makeReadySJF(pcb.initQueue, cpuTime);
+        pcb.printReadyQueue();
+
+        if (!pcb.queueEmpty(pcb.readyQueue)) {
+            if (isProcessRunning) {
+                PCB_Class::PCB shortestJob = pcb.findTagPCB(pcb.readyQueue, pcb.BURST_TAG);
+                if (shortestJob.burstTime < currentProcess.burstTime) {
+                    pcb.pushQueue(currentProcess, pcb.readyQueue);
+                    currentProcess = shortestJob;
+                    pcb.removeBlock(currentProcess, pcb.readyQueue);
                 }
-
-                block.waitTime = cpuTime - block.arrivalTime;
-                cpuTime += 1;  
-                block.burstTime -= 1;
-
-                // if process is not done, put back in ready queue
-                if (block.burstTime > 0) {
-                    pcb.pushQueue(block, pcb.readyQueue);
-                } 
-                
-                else {
-                    // process is done(?)
-                    block.exitTime = cpuTime;
-                    pcb.popQueue(pcb.readyQueue);
-                    pcb.pushQueue(block, pcb.doneQueue);
-                }
+            } else {
+                currentProcess = pcb.findTagPCB(pcb.readyQueue, pcb.BURST_TAG);
+                pcb.removeBlock(currentProcess, pcb.readyQueue);
+                isProcessRunning = true;
             }
         }
 
-        printOutput(verbose, pcb);
+        // update process wait times in ready queue
+        std::queue<PCB_Class::PCB> tempQueue;
+        while (!pcb.queueEmpty(pcb.readyQueue)) {
+            PCB_Class::PCB processInQueue = pcb.getPCB(pcb.readyQueue);
+            processInQueue.waitTime++;
+            tempQueue.push(processInQueue);
+            pcb.popQueue(pcb.readyQueue);
+        }
+
+        // swap tempqueue and actual queue
+        while (!tempQueue.empty()) {
+            pcb.pushQueue(tempQueue.front(), pcb.readyQueue);
+            tempQueue.pop();
+        }
+
+        if (isProcessRunning) {
+            currentProcess.burstTime--;
+
+            // debug output 
+            cout << "Debug: Process P_" << currentProcess.pid << " - CPU Time: " << cpuTime
+                 << ", Arrival Time: " << currentProcess.arrivalTime
+                 << ", Wait Time: " << currentProcess.waitTime
+                 << ", Burst Time: " << currentProcess.burstTime << endl;
+
+            if (currentProcess.burstTime == 0) {
+                // Set the exitTime when the process is completed
+                currentProcess.exitTime = cpuTime + 1;  // Update the exitTime
+                pcb.pushQueue(currentProcess, pcb.doneQueue); 
+                isProcessRunning = false;
+            }
+
+        }
+
+        cpuTime++;
     }
+    printOutput(verbose, pcb);
 }
+
+
+
 
 
 
@@ -99,11 +121,18 @@ void SJF::printOutput(bool verbose, PCB_Class& pcb){
 
         pcb.popQueue(pcb.doneQueue);
     }
+
     cout << "\n\tAverage Wait: " << (totalWait/(float) processes) << endl; 
 }
 
-void SJF::verboseOutput(PCB_Class::PCB block){
-    int cpuTime = block.waitTime + block.arrivalTime;
-    cout << " \tCPU Entered: " << cpuTime
-        << " CPU Left: " << (block.burstTime + cpuTime);
+
+void SJF::verboseOutput(PCB_Class::PCB block) {
+        << "\tCPU Entered: " << block.enterTime;
+        cout << "\tCPU Left: " << block.exitTime;
+
 }
+
+
+
+
+
