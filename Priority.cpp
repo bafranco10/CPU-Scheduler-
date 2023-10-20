f3e10cbf46ea36968aa9a0e1c9395a13f982e0bb
Brian Franco <104641779+bafranco10@users.noreply.github.com>
2023-10-20 19:42:41 -0400

    Add files via upload

--- Priority.cpp
+++ Priority.cpp
@@ -17,27 +17,36 @@ Priority::Priority(){
 Priority::~Priority(){
 }
 
+// takes in verbose flag, preemptive flag, and a set of pcbs
+// it then returns average wait time of processes scheduled with a priority queue depending on preemption flag
 void Priority::prioritySchedule(bool verbose, bool preemption, PCB_Class& pcb){
-    PCB_Class::PCB block;
+  PCB_Class::PCB block;
     FCFS fcfs;
     int cpuTime = 0;
-    int initSize = pcb.queueSize(pcb.initQueue); //need to initialize size of the queue to be used
-
+    int initSize = pcb.queueSize(pcb.initQueue);
+    // if not preemptive use fcfs sorted by priority
     if(!preemption){
         fcfs.fcfsSchedule(verbose,pcb,pcb.PRIORITY_TAG);
     }
+    
+    // sort by finding item with highest priority and then check priorities again after completion of a process
+
     else{
       int cpuTime = 0;
       PCB_Class::PCB currentProcess;
       bool isProcessRunning = false;
       
+      // sort processes by arrival time
       pcb.queueTagSort(pcb.initQueue, pcb.ARRIVAL_TAG);
       
       while (!pcb.queueEmpty(pcb.initQueue) || !pcb.queueEmpty(pcb.readyQueue) || isProcessRunning) {
-
-        // Move processes from initQueue to readyQueue based on arrival time
+	// Move processes from initQueue to readyQueue based on arrival time
         pcb.makeReadySJF(pcb.initQueue, cpuTime);
-        pcb.printReadyQueue();
+
+	if (verbose) {
+
+	  cout << "P_" << currentProcess.pid << " Enter: " << cpuTime;
+        }
 	
         if (!pcb.queueEmpty(pcb.readyQueue)) {
 	  if (isProcessRunning) {
@@ -47,9 +56,12 @@ void Priority::prioritySchedule(bool verbose, bool preemption, PCB_Class& pcb){
 	    if (highestPriority.priority > currentProcess.priority) {
 	      pcb.pushQueue(currentProcess, pcb.readyQueue);
 	      currentProcess = highestPriority;
+	      
 	      pcb.removeBlock(currentProcess, pcb.readyQueue);
 	    }
-	  } else {
+	  }
+	  // else currentProcess stays the same
+	  else {
 	    currentProcess = pcb.findTagPCB(pcb.readyQueue, pcb.PRIORITY_TAG);
 	    pcb.removeBlock(currentProcess, pcb.readyQueue);
 	    isProcessRunning = true;
@@ -59,10 +71,14 @@ void Priority::prioritySchedule(bool verbose, bool preemption, PCB_Class& pcb){
         // update process wait times in ready queue
         std::queue<PCB_Class::PCB> tempQueue;
         while (!pcb.queueEmpty(pcb.readyQueue)) {
-            PCB_Class::PCB processInQueue = pcb.getPCB(pcb.readyQueue);
-            processInQueue.waitTime++;
-            tempQueue.push(processInQueue);
-            pcb.popQueue(pcb.readyQueue);
+	  PCB_Class::PCB processInQueue = pcb.getPCB(pcb.readyQueue);
+	  
+	  if (processInQueue.pid != currentProcess.pid) {
+	    processInQueue.waitTime++;
+	  }
+	    
+	  tempQueue.push(processInQueue);
+	  pcb.popQueue(pcb.readyQueue);
         }
 	
         // swap tempqueue and actual queue
@@ -74,55 +90,43 @@ void Priority::prioritySchedule(bool verbose, bool preemption, PCB_Class& pcb){
         if (isProcessRunning) {
 	  currentProcess.burstTime--;
 	  
-	  // debug output 
-	  cout << "Debug: Process P_" << currentProcess.pid << " - CPU Time: " << cpuTime
-	       << ", Arrival Time: " << currentProcess.arrivalTime
-	       << ", Wait Time: " << currentProcess.waitTime
-                 << ", Burst Time: " << currentProcess.burstTime << endl;
-	  
 	  if (currentProcess.burstTime == 0) {
 	    // Set the exitTime when the process is completed
 	    currentProcess.exitTime = cpuTime + 1;  // Update the exitTime
+	    
 	    pcb.pushQueue(currentProcess, pcb.doneQueue); 
 	    isProcessRunning = false;
+	  }
+	  if (verbose) {
+	    cout << " Exit: " << currentProcess.exitTime << endl;
 	  }
 	  
         }
 	
         cpuTime++;
       }
-      printOutput(verbose, pcb);
+      printOutput(pcb);
     }
     
 }
 
-void Priority::printOutput(bool verbose, PCB_Class& pcb){
+void Priority::printOutput(PCB_Class& pcb){
+  
+  PCB_Class::PCB block;
+  int totalWait = 0, wait;
+  int processes = pcb.queueSize(pcb.doneQueue);
+  
+  pcb.queueTagSort(pcb.doneQueue,pcb.PID_TAG);
     
-    PCB_Class::PCB block;
-    int totalWait = 0, wait;
-    int processes = pcb.queueSize(pcb.doneQueue);
-    
-    pcb.queueTagSort(pcb.doneQueue,pcb.PID_TAG);
-
     while(!pcb.queueEmpty(pcb.doneQueue)){
-        block = pcb.getPCB(pcb.doneQueue);
-        cout << "\nP_" << block.pid;
-
+      block = pcb.getPCB(pcb.doneQueue);
+      cout << "\nP_" << block.pid;
+      
         wait = block.waitTime;
         totalWait += wait;
         cout << "\tWait: " << wait;
-
-        if(verbose){
-            verboseOutput(block);
-        }
-
+	
         pcb.popQueue(pcb.doneQueue);
     }
     cout << "\n\tAverage Wait: " << (totalWait/(float) processes) << endl; 
-}
-
-void Priority::verboseOutput(PCB_Class::PCB block){
-    int cpuTime = block.waitTime + block.arrivalTime;
-    cout << " \tCPU Entered: " << cpuTime
-        << " CPU Left: " << (block.burstTime + cpuTime);
 }
